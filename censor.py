d915839a0320caa80e144c14cf301a4267ceea09
Daniel Morgan <daniel.morgan@allscripts.com>
2019-08-16 15:28:56 -0500

    Pushing changes to Master, creating new branch to avoid losing functionality

--- censor.py
+++ censor.py
@@ -15,13 +15,13 @@ email_four = open("email_four.txt", "r").read()
 
 #print(censor_algorithms())
 
-proprietary_terms = ["she", "She" "personality", "matrix", "sense", "of", "self", "self-preservation", "learning", "algorithms", " her", "herself"]
+proprietary_terms = ["she", "She", "personality", "matrix", "sense", "of", "self", "self-preservation", "learning", "algorithms", " her", "herself"]
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out", "control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
 def censor_email_one(originalString, valueToCensor, valueToReplace):
     stringToReturn = originalString.replace(valueToCensor, valueToReplace)
     return stringToReturn
-#print(email_two)
+#print(censor_email_one(email_one))
 
 def censor_email_two(email):
     email_two_split = email.split()
@@ -37,27 +37,24 @@ def censor_email_two(email):
 negative_words_repeated = []
 
 def censor_email_three(email):
+    negative_words_repeated = []
     email_three_split = email.split()   
-    for terms in proprietary_terms:
-        for word in email_three_split:
-            if terms == word:
-                word_index = email_three_split.index(word)
-                email_three_split[word_index] = "*"*len(word)
+
     for term in negative_words:
         for word in email_three_split:
             if term == word:
-                negative_words_repeated += term
+                negative_words_repeated.append(term)
                 for bad_word in negative_words_repeated:
                     if bad_word == term:
                         email_three_split[word_index] = "*"*len(word)
                     else:
-                        return term
-    return " ".join(email_three_split)          
+                       return term
+
+    for terms in proprietary_terms: #censors the words in the list proprietary_terms
+        for word in email_three_split:
+            if terms == word:
+                word_index = email_three_split.index(word)
+                email_three_split[word_index] = "*"*len(word)
+    return " ".join(email_three_split)         
 
 print(censor_email_three(email_three))
-        
-#1. Check email for negative words
-#    A. If email contains negative word check the list
-#        a. If list doesn't contain word, add it
-#        b. If list contains word, censor it
-#2. Censor words from proprietary_words
